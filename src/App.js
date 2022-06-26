db87f5edd58b60c2e895e8d81a151d172ea74d4a
sonu saini <sonusaini6280@gmail.com>
2022-06-26 15:50:28 +0530

    manage user with profile context put real time subs

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
 import Home from './pages/Home';
 import SigIn from './pages/SigIn';
 import './styles/main.scss';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SigIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SigIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
--- src/pages/SigIn.js
+++ src/pages/SigIn.js
@@ -1,55 +1,47 @@
 import React from 'react';
 import firebase from 'firebase/app';
-import { Container, Grid, Row, Panel, Button, Icon, Alert } from 'rsuite';
-import Col from 'rsuite/lib/Col';
+import { Col, Container, Grid, Panel, Row, Button, Icon, Alert } from 'rsuite';
 import { auth, database } from '../misc/firebase';
 
-const SigIn = () => {
-  const SignInWithProvider = async provider => {
+const SignIn = () => {
+  const signInWithProvider = async provider => {
     try {
       const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
-
-      if (additionalUserInfo.isNewuser) {
+      if (additionalUserInfo.isNewUser) {
         await database.ref(`/profiles/${user.uid}`).set({
           name: user.displayName,
-          createAt: firebase.database.ServerValue.TIMESTAMP,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
         });
       }
-
-      Alert.success('Signed In', 4000);
+      Alert.success('Sigend in', 4000);
     } catch (err) {
-      Alert.info(err.message, 4000);
+      Alert.error(err.message, 4000);
     }
   };
-
-  const onFacebookSigIn = () => {
-    SignInWithProvider(new firebase.auth.FacebookAuthProvider());
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
   };
 
-  const onGoogleSigIn = () => {
-    SignInWithProvider(new firebase.auth.GoogleAuthProvider());
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
   };
 
   return (
     <Container>
       <Grid className="mt-page">
         <Row>
-          <Col xs={24} md={12} mdoffset={6}>
+          <Col xs={24} md={12} mdOffset={6}>
             <Panel>
               <div className="text-center">
-                <h2>welcome to chat</h2>
-                <p>Progressive chat platform for neophytes</p>
+                <h2>Welcome to Chat</h2>
+                <p>Progressive chat program for neophytes</p>
               </div>
-
               <div className="mt-3">
-                <Button block color="blue" onClick={onFacebookSigIn}>
-                  <Icon icon="facebook" />
-                  Continue with Facebook
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
                 </Button>
-
-                <Button block color="green" onClick={onGoogleSigIn}>
-                  <Icon icon="google" />
-                  Continue with Google
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
                 </Button>
               </div>
             </Panel>
@@ -60,4 +52,4 @@ const SigIn = () => {
   );
 };
 
-export default SigIn;
+export default SignIn;

f95b94d7c7ccc6a74ab1b363a4e9f0d63bb361cf
sonu saini <sonusaini6280@gmail.com>
2022-06-28 00:53:07 +0530

    added sidebar and usemediaquery in pages

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, Icon, Drawer } from 'rsuite';
+import Dashboard from '.';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+
+const DashboardToggle = () => {
+  const { isOpen, close, open } = useModalState();
+  const isMobile = useMediaQuery('(max-width: 992px)');
+
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" />
+        Dashboard
+      </Button>
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  );
+};
+
+export default DashboardToggle;

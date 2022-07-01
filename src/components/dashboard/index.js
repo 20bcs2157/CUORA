6988798a3c6e5dcd450375421c3a7dcea2a78354
sonu saini <sonusaini6280@gmail.com>
2022-07-01 14:52:47 +0530

    update all user references and profile

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -5,17 +5,21 @@ import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
 import AvatarUploadBtn from './AvatarUploadBtn';
 import ProviderBlock from './ProviderBlock';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
-
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
 
       Alert.success('Nickname has been updted', 4000);
     } catch (err) {

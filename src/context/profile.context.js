5f32a1473ede6e8e3f78c3b320acf4351e20dec0
sonu saini <sonusaini6280@gmail.com>
2022-07-01 16:34:54 +0530

    add real time persence a user

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,5 +1,16 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 const ProfileContext = createContext();
 
@@ -9,10 +20,13 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let userRef;
+    let userStatusRef;
 
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
+
         userRef.on('value', snap => {
           const { name, createdAt, avatar } = snap.val();
 
@@ -23,31 +37,50 @@ export const ProfileProvider = ({ children }) => {
             uid: authObj.uid,
             email: authObj.email,
           };
-
           setProfile(data);
           setIsLoading(false);
         });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (!!snapshot.val() === false) {
+            return;
+          }
+
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (userRef) {
-          userRef.off();
+          userRef.ofF();
+        }
+        if (userStatusRef) {
+          userStatusRef.off();
         }
 
+        database.ref('.info/connected').off();
         setProfile(null);
         setIsLoading(false);
       }
     });
-
     return () => {
       authUnsub();
+      database.ref('.info/connected').off();
 
       if (userRef) {
-        userRef.off();
+        userRef.ofF();
+      }
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
-  });
+  }, []);
 
   return (
-    <ProfileContext.Provider value={{ profile, isLoading }}>
+    <ProfileContext.Provider value={{ isLoading, profile }}>
       {children}
     </ProfileContext.Provider>
   );
